0a664d63edf519ae0d955e5cae5429e9c0b7e7bb
basiljoy91 <basiljoygreen@gmail.com>
2026-05-01 21:49:39 +0530

    fix: correct typo in "Browse Collections" link text

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -14,7 +14,7 @@ export default function NotFound() {
               Go Home
             </Link>
             <Link href="/collections" className="secondary-button">
-              Browse Colle
+              Browse Collec
             </Link>
           </div>
         </div>
